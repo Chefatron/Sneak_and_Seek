0182405030dfa7c21efabc29ae9ae9251acefb31
CptInori <128365775+CptInori@users.noreply.github.com>
2023-10-30 17:20:21 +0000

    Path Finding continuation
    More changes made to the pathfinding scripts (nearly done)

--- Sneak and Seek/Assets/Scripts/Ben's Scripts/PathFinding/PathFinding.cs
+++ Sneak and Seek/Assets/Scripts/Ben's Scripts/PathFinding/PathFinding.cs
@@ -3,28 +3,127 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
 
 public class PathFinding : MonoBehaviour
 {
+    public ConnectionMatrix _connectionMatrix; 
     public GameObject nodeMatrix;
     private GameObject[] nodeList;
-    public int[] path;
-    NavMeshAgent target;
-    bool alternateTarget;
 
+    NavMeshAgent target;
+
+    bool alternateTarget = false;
+    bool changeTarget = false;
+
+    int[] path;
+    int sourceNode;
+
+
+    int DijkstarAlgorithm(ConnectionMatrix connectionMatrix, int Nodes, int SourceNode, int targetNode)
+    {
+        int[] DistancePerNode = new int[Nodes];
+        int[] PredictedWeightPerNode = new int[Nodes];
+        int[] VisitedNodes = new int[Nodes];
+        int Counter;
+        int MinimumDistance;
+        int NextNode = 0;
+
+        int startNode, endNode;
+
+        if(SourceNode > targetNode)
+        {
+            startNode = targetNode;
+            endNode = SourceNode;
+        }
+        else
+        {
+            startNode = SourceNode;
+            endNode = targetNode;
+        }
+
+        for (int i = startNode; i < endNode; i++)
+        {
+            DistancePerNode[i] = connectionMatrix.connectionMatrix[SourceNode, i];
+            PredictedWeightPerNode[i] = SourceNode;
+            VisitedNodes[i] = 0;
+        }
+
+        DistancePerNode[SourceNode] = 0;
+        VisitedNodes[SourceNode] = 1;
+        Counter = 1;
+
+        while (Counter < (endNode - 1))
+        {
+            MinimumDistance = 999;
+
+            for(int i = startNode; i < endNode; i++) 
+            {
+                if ((DistancePerNode[i] < MinimumDistance) && (VisitedNodes[i] == 0))
+                {
+                    MinimumDistance = DistancePerNode[i];
+                    NextNode = i;
+                }
+            }
+
+            VisitedNodes[NextNode] = 1;
+
+            for (int i = startNode; i < endNode; i++)
+            {
+                if (VisitedNodes[i] == 0)
+                {
+                    if (MinimumDistance + connectionMatrix.connectionMatrix[NextNode, i] < DistancePerNode[i])
+                    {
+                        DistancePerNode[i] = MinimumDistance + connectionMatrix.connectionMatrix[NextNode, i];
+
+                        PredictedWeightPerNode[i] = NextNode;
+                    }
+                }
+            }
+
+            Counter++;
+        }
+
+        return NextNode;
+    }
     // Start is called before the first frame update
     void Start()
     {
+        sourceNode = 0;
+
+        path = new int[4] { 2, 6, 8, 3 };
+
         nodeList = new GameObject[nodeMatrix.transform.childCount];
-        path = new int[] { 2, 7, 9, 4 };                                // The list value will have to be one higher than the actual ID to account for the array starting at 0
+
+        for (int i = 0; i < nodeMatrix.transform.childCount; i++)
+        {
+            nodeList[i] = nodeMatrix.transform.GetChild(i).gameObject;
+        }
+
+        target.destination = nodeList[0].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (alternateTarget!)
+        if (transform.position == nodeList[sourceNode].transform.position)
         {
-            Target.destination = nodeList[];
+            changeTarget = true;
         }
+
+        if (!changeTarget)
+        {
+            target.destination = nodeList[DijkstarAlgorithm(_connectionMatrix, nodeList.Length, path[sourceNode], path[sourceNode + 1])].transform.position;
+        }
+        if (changeTarget) 
+        {
+            sourceNode++;
+
+            if (sourceNode > 3)
+            {
+                sourceNode = 0;
+            }
+        }
+
     }
 }
