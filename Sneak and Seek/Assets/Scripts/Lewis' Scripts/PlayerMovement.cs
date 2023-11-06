ee5ab8f31567a7fd7a3404d8caaa75edb1f49720
Finleyfooonfire <finleyfooonfire@gmail.com>
2023-11-06 12:59:00 +0000

    Code by Ben
    Some code he was working on before the lesson for animation controlers

--- Sneak and Seek/Assets/Scripts/Lewis' Scripts/PlayerMovement.cs
+++ Sneak and Seek/Assets/Scripts/Lewis' Scripts/PlayerMovement.cs
@@ -9,15 +9,26 @@ public class PlayerMovement : MonoBehaviour
 
     Rigidbody playerRB;
 
+    public GameObject sprite;
+
     Vector3 movement;
 
     Vector2 stickInputData;
 
     public int speed;
 
+    float timer;
+
+    string state;
+    string nextState;
+
     // Start is called before the first frame update
     void Start()
     {
+        state = "Hidden";
+
+        timer = 120;
+
         playerRB = GetComponent<Rigidbody>();
 
         controls = new MasterInput();
@@ -25,6 +36,8 @@ public class PlayerMovement : MonoBehaviour
         controls.Enable();
 
         controls.Player.Movement.Enable();
+
+        //sprite = GetComponentInChild<GameObject>();
     }
 
     // Is called when left stick on controller is used
@@ -34,12 +47,48 @@ public class PlayerMovement : MonoBehaviour
         
         // Processing input data for physics based movement
         stickInputData = stickInput.Get<Vector2>();
+        sprite.transform.scale(new Vector3(stickInputData.x, 0f, stickInputData.y));
         movement = new Vector3(stickInputData.x, 0f, stickInputData.y);
+    }
+
+    void Seen()
+    {
+        timer = 120;
+        state = "Seen";
+        nextState = "Searching";
+    }
+
+    void Searching()
+    {
+        timer = 120;
+        state = "Searching";
+        nextState = "Hidden";
+    }
+
+    void Hidden()
+    {
+        timer = 120;
+        state = "Hidden";
     }
 
     // Runs physics update for movement
     void FixedUpdate()
     {
+        if (timer > 0)
+        {
+            timer -= Mathf.RoundToInt(Time.deltaTime);
+            if (timer == 0)
+            {
+                if (state == "Seen")
+                {
+                    Searching();
+                }
+                else if (state == "Searching")
+                {
+                    Hidden();
+                }
+            }
+        }
         // Applying physics based movement
         playerRB.AddForce(movement * speed);
     }
