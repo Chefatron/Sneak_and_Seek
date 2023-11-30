254d6a5dc79da5e62092ec6b121e8283533f4a6a
CptInori <128365775+CptInori@users.noreply.github.com>
2023-11-30 11:38:37 +0000

    Changes for Beta
    Loading scene added
    loading animations available
    maid assets added

--- Sneak and Seek/Assets/Scripts/Lewis' Scripts/FloorChange.cs
+++ Sneak and Seek/Assets/Scripts/Lewis' Scripts/FloorChange.cs
@@ -11,8 +11,24 @@ public class FloorChange : MonoBehaviour
     // The name of the scene for the stairs to load
     [SerializeField] int sceneIndex;
 
+    IEnumerator loadNextScene(int nextScene)
+    {
+        SceneManager.LoadScene("LoadingScene");
+        AsyncOperation asyncLoaing = SceneManager.LoadSceneAsync(nextScene);
+
+        while (!asyncLoaing.isDone) 
+        {
+            yield return null;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        gameManager.LoadScene(sceneIndex);
+        loadNextScene(sceneIndex);
+    }
+
+    public void OnButtonPress()
+    {
+        loadNextScene(sceneIndex);
     }
 }
